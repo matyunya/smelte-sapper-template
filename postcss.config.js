070a4eb1f23eec834e3d15929ca3f43890423620
Maxim Matyunin <matyunya@gmail.com>
2019-07-31 11:09:34 +0900

    fixes

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,14 @@
 const production = !process.env.ROLLUP_WATCH;
-const purgecss = require('@fullhuman/postcss-purgecss');
+const purgecss = require("@fullhuman/postcss-purgecss");
 
 module.exports = {
   plugins: [
-    require('postcss-import')(),
-    require('tailwindcss')('./node_modules/smelte/tailwind.config.js'),
-    require('postcss-nesting'),
-    require('autoprefixer'),
+    require("postcss-import")(),
+    require("tailwindcss")("./node_modules/smelte/tailwind.config.js"),
+    require("autoprefixer"),
     production &&
       purgecss({
-        content: ['./**/*.html', './**/*.svelte'],
+        content: ["./**/*.html", "./**/*.svelte"],
         defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
       })
   ]
